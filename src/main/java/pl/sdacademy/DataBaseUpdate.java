6ccfbe62697e0a4944cbffd54244afbcffea9090
wkargul <wojciech.kargul@gmail.com>
2021-05-26 20:40:36 +0200

    final release on 26052021

--- src/main/java/pl/sdacademy/DataBaseUpdate.java
+++ src/main/java/pl/sdacademy/DataBaseUpdate.java
@@ -6,18 +6,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import pl.sdacademy.model.dao.WeatherAPIDatabase;
 import pl.sdacademy.utils.ReadFileToStringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DataBaseUpdate {
     public static void main(String[] args) {
         String json = ReadFileToStringUtils.getFileFromResourceAsString("current.city.list.json");
-        WeatherAPIDatabase[] weatherAPIDatabases = update(json);
-        for (WeatherAPIDatabase weatherAPIDatabase : weatherAPIDatabases) {
-            System.out.println(weatherAPIDatabase);
-        }
+//        WeatherAPIDatabase[] weatherAPIDatabases = mapper(json);
+//        for (WeatherAPIDatabase weatherAPIDatabase : weatherAPIDatabases) {
+//            System.out.println(weatherAPIDatabase);
+//        }
+        update(json);
         System.out.println("Baza danych zostałą zaktualizowana");
     }
 
-    private static WeatherAPIDatabase[] update(String json) {
-
+    private static WeatherAPIDatabase[] mapper(String json) {
         try {
             ObjectMapper mapper = new ObjectMapper()
                     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -28,6 +32,42 @@ public class DataBaseUpdate {
             e.printStackTrace();
         }
         return new WeatherAPIDatabase[0];
+    }
+
+    private static boolean update(String json) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("weatherstationwithapiPU");
+        final EntityManager em = emf.createEntityManager();
+        boolean result = false;
+        WeatherAPIDatabase[] weatherAPIDatabases = mapper(json);
+
+        try {
+            em.getTransaction().begin();
+            for (WeatherAPIDatabase weatherAPIDatabase : weatherAPIDatabases) {
+                String cityName;
+                if (weatherAPIDatabase.getLangs() == null) {
+                    cityName = weatherAPIDatabase.getName();
+                } else {
+                    cityName = weatherAPIDatabase.getLangs().stream()
+                            .filter(f -> f.containsKey("pl"))
+                            .map(f -> f.get("pl"))
+                            .findFirst()
+                            .orElse(weatherAPIDatabase.getName());
+                }
+
+                WeatherAPIDatabase cityToDb = new WeatherAPIDatabase();
+                cityToDb.setName(cityName);
+                cityToDb.setCountry(weatherAPIDatabase.getCountry());
+                cityToDb.setId(weatherAPIDatabase.getId());
+
+                em.persist(cityToDb);
+                result = true;
+
+            }
+            em.getTransaction().commit();
+            return result;
+        } finally {
+            emf.close();
+        }
     }
 
 }
